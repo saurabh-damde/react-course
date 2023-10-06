44e02e39368bb18bca7c81ecc208d57977e17c15
Saurabh Damde <saurabhdamde@gmail.com>
2023-10-06 15:55:27 +0530

    Initial Project Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import keyConceptsImage from "./assets/images/key-concepts.png";
+import componentsImage from "./assets/images/components.png";
+import stateImage from "./assets/images/state.png";
+import eventsImage from "./assets/images/events.png";
 
-function App() {
+const concepts = [
+  {
+    title: "Components",
+    image: componentsImage,
+    description:
+      "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
+  },
+  {
+    title: "State",
+    image: stateImage,
+    description:
+      "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
+  },
+  {
+    title: "Events",
+    image: eventsImage,
+    description:
+      "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt="Medal Badge with a Star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
       </header>
+      <ul id="concepts">
+        <li className="concept">
+          <img src={concepts[0].image} alt="Cubical Building Block Structure" />
+          <h2>{concepts[0].title}</h2>
+          <p>{concepts[0].description}</p>
+        </li>
+        <li className="concept">
+          <img
+            src={concepts[1].image}
+            alt="Cylindrical Building Block Structure"
+          />
+          <h2>{concepts[1].title}</h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[2].image} alt="Speaker" />
+          <h2>{concepts[2].title}</h2>
+          <p>{concepts[2].description}</p>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
