06fac39f0c3e91f70304ac805932e37c17ea9dd8
Saurabh Damde <saurabhdamde@gmail.com>
2023-10-06 13:34:11 +0530

    Implement component composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
@@ -8,13 +9,13 @@ function ExpenseItem(props) {
     props.expense.amount,
   ];
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem expense={props.expenses[0]} />
       <ExpenseItem expense={props.expenses[1]} />
       <ExpenseItem expense={props.expenses[2]} />
       <ExpenseItem expense={props.expenses[3]} />
-    </div>
+    </Card>
   );
 }
 export default Expenses;
