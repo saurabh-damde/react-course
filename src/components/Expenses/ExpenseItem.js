1406cf97d61a5b3f9571c620b007c33bbb89a2aa
Saurabh Damde <saurabhdamde@gmail.com>
2023-10-09 10:14:57 +0530

    Implement State & Event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,17 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [date, title, amount] = [
-    props.expense.date,
-    props.expense.title,
-    props.expense.amount,
-  ];
+  let [date, amount] = [props.expense.date, props.expense.amount];
+
+  const [title, setTitle] = useState(props.expense.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -15,6 +19,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
