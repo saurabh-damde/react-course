05dd54621c4665309a60f452f7140e3dad97b791
Saurabh Damde <saurabhdamde@gmail.com>
2023-10-13 14:19:11 +0530

    Split the App into Components

---
+++ src/components/Table/Table.js
@@ -0,0 +1,13 @@
+import TableHeader from "./TableHeader";
+import TableBody from "./TableBody";
+
+const Table = () => {
+  return (
+    <table className="result">
+      <TableHeader />
+      <TableBody />
+    </table>
+  );
+};
+
+export default Table;
---
+++ src/components/Table/TableBody.js
@@ -0,0 +1,15 @@
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>YEAR NUMBER</td>
+        <td>TOTAL SAVINGS END OF YEAR</td>
+        <td>INTEREST GAINED IN YEAR</td>
+        <td>TOTAL INTEREST GAINED</td>
+        <td>TOTAL INVESTED CAPITAL</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default TableBody;
